2c739c5815e5ec0b1853ee1a52030315e23eb9ba
Nurmyrat AMANMADOV <amanmadov@gmail.com>
2022-12-30 12:34:29 -0800

    add Time Complexity of Javascript array Methods for notes

--- AlgoExpert Problems.js
+++ AlgoExpert Problems.js
@@ -448,4 +448,65 @@ if(!array.length)
 
 //#endregion
 
+//#region Time Complexity of Javascript array Methods
+
+// Mutator Methods
+
+    // 1. push() - 0(1)
+    // Add a new element to the end of the array.
+
+    // 2. pop() - 0(1)
+    // Delete the last element of the array
+
+    // 3. shift() - 0(n)
+    // Delete the first element of the array
+
+    // 4. unshift() - 0(n)
+    // Add one or more elements in the beginning of the array
+
+    // 5. splice() - 0(n)
+    // Remove, add or replace a new element indicate by index.
+
+    // 6. sort() - 0(n log(n))
+    // Modify the array, ordered by a compare Function.
+
+
+// Accessor methods
+
+    // 1. concat() - 0(n)
+    // Create a new array with the union of two or more arrays.
+
+    // 2. slice() - 0(n)
+    // Return a copy of a sub array between two index, start and end.
+    // Important Note: if you modify the original array, the value also will be modify in the copy array.
+
+    // 3. indexOf() - 0(n)
+    // Return the first index of the element that exists in the array, and if not exists return-1.
+
+
+// Iteration methods
+
+    // 1. forEach() - 0(n)
+    // Just execute a function for each element in the array.
+
+    // 2. map() - 0(n)
+    // Create a new array with the result of the callback function (this function is executed for each item same as forEach)
+
+    // 3. filter() - 0(n)
+    // Create a new array with the elements that apply the given filter condition as true.
+
+    // 4. reduce() - 0(n)
+    // Return a single value after applying the reduction function for each element.
+
+
+// Other methods
+
+    // 1. some() - 0(n)
+    // Return a boolean value as true if found one or more item that apply the given condition, and return false if not (also if the array is empty).
+    
+    // 2. every() - 0(n)
+    // This function Return a boolean value as true if all the items apply the given condition, and false if not.
+
+//#endregion
+
 //#endregion
