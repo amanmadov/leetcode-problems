58459f330d9e30b461cb95557c2edf2f75189b3e
Nurmyrat AMANMADOV <amanmadov@gmail.com>
2023-01-01 15:35:18 -0800

    added alternate solution to caesarCipherEncryptor

--- AlgoExpert Problems.js
+++ AlgoExpert Problems.js
@@ -225,6 +225,23 @@ function caesarCipherEncryptor(string, key) {
     return output;
 }
 
+// alternate approach O(n) time | O(n) space
+function caesarCipherEncryptor(string, key) {
+    const newLetters = [];
+    const newKey = key % 26;
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    for (const letter of string) {
+        newLetters.push(getNewLetter(letter,newKey,alphabet));
+    }
+    return newLetters.join('');
+
+    function getNewLetter(letter, key, alphabet) {
+        const newLetterCode = alphabet.indexOf(letter) + key;
+        return alphabet[newLetterCode % 26];
+    }
+}
+
 console.log(caesarCipherEncryptor('xyz',2));
 
 //#endregion
